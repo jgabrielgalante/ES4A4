69a3cec90e56b4aeab765548b05de5d69d134b6a
João Gabriel Galante <jgabrielgalante@gmail.com>
2022-06-07 19:44:45 -0300

    Adição do projeto back-end

---
+++ back/src/main/java/br/edu/ifsp/terraqueos/comentesobre/ComenteSobreApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.terraqueos.comentesobre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComenteSobreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComenteSobreApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/br/edu/ifsp/terraqueos/comentesobre/ComentesobreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.terraqueos.comentesobre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComentesobreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
