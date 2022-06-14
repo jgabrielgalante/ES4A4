777694d29c16de1caf10a5982b05c08e77f30043
henrique souza <hi@henriqueo.com>
2022-06-14 19:19:01 -0300

    Test: slug generation

--- server/core/tests.py
+++ server/core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
+from . import models
+
 # Create your tests here.
+class SlugTestCase(TestCase):
+    def test_gen_slug(self):
+        self.assertEqual(models.gen_slug('Sanctus dominus'), 'sanctus-dominus')
+
